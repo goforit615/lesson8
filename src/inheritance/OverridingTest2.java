224483408e8e5259d580283f9b7934e48fc828e1
goforit615 <bupark82@gsm.hs.kr>
2025-02-23 16:25:08 +0900

    OverridingTest3

--- src/inheritance/OverridingTest2.java
+++ src/inheritance/OverridingTest2.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class OverridingTest2 {
     public static void main(String[] args) {
-        Customer vc = new VIPCustomer(10030, "나몰라", 2000);
+        Customer vc = new VIPCustomer(10030, "계백", 2000);
         vc.bonusPoint = 1000;
 
         System.out.println(vc.getCustomerName() + " 님이 지불해야 하는 금액은 " + vc.calcPrice(10000) + "원입니다.");
