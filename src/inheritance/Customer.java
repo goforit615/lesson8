e8c1fa77dc20a9ceb681f8e261e8b8c0ee2e4195
goforit615 <bupark82@gsm.hs.kr>
2025-02-23 14:58:18 +0900

    add Customer constructor

--- src/inheritance/Customer.java
+++ src/inheritance/Customer.java
@@ -7,10 +7,12 @@ public class Customer {
     int bonusPoint; // 보너스 포인트
     double bonusRatio;  // 적립 비율
 
-    public Customer() {
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
         customerGrade = "SILVER";   // 기본 등급
         bonusRatio = 0.01;  // 보너스 포인트 기본 적립 비율
-        System.out.println("Customer() 생성자 호출 ");
+        System.out.println("Customer(int, String) 생성자 호출 ");
     }
 
     public int calcPrice(int price) {
