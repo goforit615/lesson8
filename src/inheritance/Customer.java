6b97c27a4eee7f3d75e92dc442c856136dccc836
goforit615 <bupark82@gsm.hs.kr>
2025-02-23 14:17:28 +0900

    Customer

--- src/inheritance/Customer.java
+++ src/inheritance/Customer.java
@@ -1,9 +1,9 @@
 package inheritance;
 
 public class Customer {
-    private int customerID; // 고객 아이디
-    private String customerName;    // 고객 이름
-    private String customerGrade;   // 고객 등급
+    protected int customerID; // 고객 아이디
+    protected String customerName;    // 고객 이름
+    protected String customerGrade;   // 고객 등급
     int bonusPoint; // 보너스 포인트
     double bonusRatio;  // 적립 비율
 
@@ -19,5 +19,25 @@ public class Customer {
 
     public String showCustomerInfo() {
         return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는" + bonusPoint + "입니다.";
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
     }
 }
